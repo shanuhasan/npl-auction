670371855c08a3b6317f6af51e2d28cf7f620d16
Shahnawaj <shahnawaj@inspheresolutions.com>
2026-07-11 14:48:54 +0530

    update

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Run the queue worker every minute and stop when jobs are done
+Schedule::command('queue:work --stop-when-empty')->everyMinute();
